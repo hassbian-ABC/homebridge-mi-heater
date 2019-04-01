db247b03177808a6b798b7e5b3759f7ba5b5b9dd
hassbian-ABC <37896214+hassbian-ABC@users.noreply.github.com>
2019-04-01 14:10:17 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,222 +1,190 @@
-const miio = require('miio')
+var fs = require('fs');
+const miio = require('miio');
+var Accessory, Service, Characteristic, UUIDGen;
 
-let Service, Characteristic
+module.exports = function(homebridge) {
+    if(!isConfig(homebridge.user.configPath(), "accessories", "MiHeater")) {
+        return;
+    }
 
-module.exports = homebridge => {
-  Service = homebridge.hap.Service
-  Characteristic = homebridge.hap.Characteristic
-  homebridge.registerAccessory('homebridge-mi-heater', 'MiHeater', MiHeater)
+    Accessory = homebridge.platformAccessory;
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    UUIDGen = homebridge.hap.uuid;
+
+    homebridge.registerAccessory('homebridge-mi-heater', 'MiHeater', MiHeater);
 }
 
-class MiHeater {
+function isConfig(configFile, type, name) {
+    var config = JSON.parse(fs.readFileSync(configFile));
+    if("accessories" === type) {
+        var accessories = config.accessories;
+        for(var i in accessories) {
+            if(accessories[i]['accessory'] === name) {
+                return true;
+            }
+        }
+    } else if("platforms" === type) {
+        var platforms = config.platforms;
+        for(var i in platforms) {
+            if(platforms[i]['platform'] === name) {
+                return true;
+            }
+        }
+    } else {
+    }
+    
+    return false;
+}
 
-  constructor(log, config) {
-    this.log = log
-    this.ip = config.ip
-    this.token = config.token
-    this.name = config.name || 'MiHeater'
+function MiHeater(log, config) {
+    if(null == config) {
+        return;
+    }
 
-	
-	
-	this.services = []
+    this.log = log;
+    this.config = config;
+    
+    this.log.info("[MiHeater][INFO]***********************************************************");
+    this.log.info("[MiHeater][INFO]          MiHeater v%s by hassbian-ABC 0.0.1");
+    this.log.info("[MiHeater][INFO]  GitHub: https://github.com/hassbian-ABC/homebridge-MiHeater ");
+    this.log.info("[MiHeater][INFO]                                                                  ");
+    this.log.info("[MiHeater][INFO]***********************************************************");
+    this.log.info("[MiHeater][INFO]start success...");
 
-    if (!this.ip)
-      throw new Error('Your must provide IP address of the MiHeater.')
 
-    if (!this.token)
-      throw new Error('Your must provide token of the MiHeater.')
-  
-    this.service = new Service.HeaterCooler(this.name)
-
-    // Active
-    this.service
-      .getCharacteristic(Characteristic.Active)
-      .on('get', this.getActive.bind(this))
-      .on('set', this.setActive.bind(this))
-	  
-	this.service
-      .getCharacteristic(Characteristic.CurrentTemperature)
-      .on('get', this.getCurrentTemperature.bind(this))
-	  
-	this.service
-      .getCharacteristic(Characteristic.CurrentHeaterCoolerState)
-      .on('get', this.getCurrentHeaterCoolerState.bind(this))
-	  .setProps({
-            validValues: [0,2]
-      })
-	  
-	this.service
-      .getCharacteristic(Characteristic.TargetHeaterCoolerState)
-      .setValue(Characteristic.TargetHeaterCoolerState.HEAT)
-	  .setProps({
-            validValues: [1]
-      })
-	  
-	this.service
-      .addCharacteristic(Characteristic.LockPhysicalControls)
-	  .on('get', this.getLockPhysicalControls.bind(this))
-	  .on('set', this.setLockPhysicalControls.bind(this))
-	  
-	this.service
-      .addCharacteristic(Characteristic.HeatingThresholdTemperature)
-      .on('get', this.getHeatingThresholdTemperature.bind(this))
-      .on('set', this.setHeatingThresholdTemperature.bind(this))
-	  .setProps({
-		  minValue: config.mintemp,
-          maxValue: config.maxtemp, 
-          minStep: 1, 
-        })
+    var that = this;
+    this.device = new miio.Device({
+        address: that.config.ip,
+        token: that.config.token
 		
-    this.service
-      .getCharacteristic(Characteristic.CurrentRelativeHumidity)
-      .on('get', this.getCurrentRelativeHumidity.bind(this))
-	  
-	this.serviceInfo = new Service.AccessoryInformation()
+    });
+}
 
-    this.serviceInfo
-      .setCharacteristic(Characteristic.Manufacturer, 'Smartmi')
-      .setCharacteristic(Characteristic.Model, 'MiHeater')
-      .setCharacteristic(Characteristic.SerialNumber, config.ip)
+MiHeater.prototype = {
+    identify: function(callback) {
+        callback();
+    },
 
-    this.services.push(this.service)
-    this.services.push(this.serviceInfo)
+    getServices: function() {
+        var that = this;
+        var services = [];
+
+        var infoService = new Service.AccessoryInformation();
+        infoService
+            .setCharacteristic(Characteristic.Manufacturer, "xiaomi")
+            .setCharacteristic(Characteristic.Model, "smartmi")
+            .setCharacteristic(Characteristic.SerialNumber, this.config.ip);
+        services.push(infoService);
+		
+	    var heaterService = new Service.HeaterCooler(this.name);
+        var currentTemperatureCharacteristic = heaterService.getCharacteristic(Characteristic.CurrentTemperature);
+        var currentHeaterCoolerStateCharacteristic = heaterService.getCharacteristic(Characteristic.CurrentHeaterCoolerState);
+	    currentHeaterCoolerStateCharacteristic.setProps({
+            validValues: [0,2]
+        });
+        var targetHeaterCoolerStateCharacteristic = heaterService.getCharacteristic(Characteristic.TargetHeaterCoolerState);
+        targetHeaterCoolerStateCharacteristic.setProps({
+            validValues: [1]
+        });
+
+        var activeCharacteristic = heaterService.getCharacteristic(Characteristic.Active);
+        var lockPhysicalControlsCharacteristic = heaterService.addCharacteristic(Characteristic.LockPhysicalControls);
+	    
+
+        var currentHumidityCharacteristic = heaterService.addCharacteristic(Characteristic.CurrentRelativeHumidity);
+		var temperatureDisplayUnitsCharacteristic = heaterService.addCharacteristic(Characteristic.TemperatureDisplayUnits);
+		temperatureDisplayUnitsCharacteristic.setValue(Characteristic.TemperatureDisplayUnits.CELSIUS)
+		temperatureDisplayUnitsCharacteristic.setProps({
+            validValues: [0]
+        });
+
+		
+		heaterService
+		   .getCharacteristic(Characteristic.HeatingThresholdTemperature)
+		   .setProps({
+		        minValue: that.config.mintemp, 
+                maxValue: that.config.maxtemp, 
+                minStep: 1, 
+            })
+		   .on('get', function(callback) {
+			   that.device.call("get_prop", ["power", "relative_humidity", "child_lock", "target_temperature", "temperature"]).then(result => {
+		            that.log.debug("[MiHeater][DEBUG]activeCharacteristic - get: " + result);
+					if (result[0] === "on") {
+						activeCharacteristic.updateValue(Characteristic.Active.ACTIVE);
+						currentHeaterCoolerStateCharacteristic.updateValue(Characteristic.CurrentHeaterCoolerState.HEATING);
+					} else if (result[0] === "off") {
+						activeCharacteristic.updateValue(Characteristic.Active.INACTIVE);
+					    currentHeaterCoolerStateCharacteristic.updateValue(Characteristic.CurrentHeaterCoolerState.INACTIVE);
+					}
+					currentHumidityCharacteristic.updateValue(result[1]);
+					currentTemperatureCharacteristic.updateValue(result[4]);
+
+					if (result[2] === "on") {
+						lockPhysicalControlsCharacteristic.updateValue(Characteristic.LockPhysicalControls.CONTROL_LOCK_ENABLED);
+					} else if (result[2] === "off") {
+						lockPhysicalControlsCharacteristic.updateValue(Characteristic.LockPhysicalControls.CONTROL_LOCK_DISABLED);
+					}
+					callback(null, result[3])
+					}).catch(function(err) {
+			    that.log.error("[MiHeater][DEBUG]activeCharacteristic - getActive Error: " + err);
+                callback(err);
+            });
+            }.bind(this))
+			.on('set', function(value, callback) {
+		         that.log.debug("[MiHeater][DEBUG]heatingThresholdTemperatureCharacteristic - set_target_temperature: " + value);
+                 that.device.call("set_target_temperature", [value]).then(result => {
+                       if(result[0] === "ok") {
+                        callback(null);
+                       } else {
+                         callback(new Error(result[0]));
+                       }            
+                    }).catch(function(err) {
+			           that.log.debug("[MiHeater][DEBUG]heatingThresholdTemperatureCharacteristic - set_target_temperature: " + err);
+                       callback(err);
+                    });
+            }.bind(this));
+					
+					
+		
+        activeCharacteristic  
+            .on('set', function(value, callback) {
+                that.device.call("set_power", [value ? "on" : "off"]).then(result => {
+			    that.log.debug("[MiHeater][DEBUG]activeCharacteristic - setActive Result: " + result);
+                    if(result[0] === "ok") {
+                        callback(null);
+                    } else {
+                        callback(new Error(result[0]));
+                    }            
+                }).catch(function(err) {
+				    that.log.error("[MiHeater][DEBUG]activeCharacteristic - setActive Error: " + err);
+                callback(err);
+            });
+        }.bind(this));
+
+		
+
+        targetHeaterCoolerStateCharacteristic.setValue(Characteristic.TargetHeaterCoolerState.HEAT);
+
+
+        lockPhysicalControlsCharacteristic
+           .on('set', function(value, callback) {
+		      that.log.debug("[MiHeater][DEBUG]lockPhysicalControlsCharacteristic - setchildlock: " + value);
+              that.device.call("set_child_lock", [value ? "on" : "off"]).then(result => {
+                  if(result[0] === "ok") {
+                     callback(null);
+                  } else {
+                     callback(new Error(result[0]));
+                  }            
+                }).catch(function(err) {
+			         that.log.debug("[MiHeater][DEBUG]lockPhysicalControlsCharacteristic - setchildlock: " + err);
+                callback(err);
+            });
+        }.bind(this));
+			
+	services.push(heaterService);
 	
-	this.discover()
-  }
+    return services;
 
-  getServices() {
-    return this.services
-  }
-
-  async discover() {
-    try {
-      this.device = await miio.device({ address: this.ip, token: this.token })
-    } catch (e) {
-      this.log.error(e)
-    }
-  }
-  
-  async getActive(callback) {
-    try {
-      const [ power ] = await this.device.call('get_prop', ['power'])
-
-      callback(
-        null,
-        (power === 'on')
-          ? Characteristic.Active.ACTIVE
-          : Characteristic.Active.INACTIVE
-      )
-    } catch (e) {
-      this.log.error('getActive', e)
-      callback(e)
-    }
-  }
-
-  async setActive(state, callback) {
-    try {
-      const power = (state === Characteristic.Active.ACTIVE)
-        ? 'on'
-        : 'off'
-
-      const [ result ] = await this.device.call('set_power', [power])
-
-      if (result !== 'ok')
-        throw new Error(result)
-
-      callback()
-    } catch (e) {
-      this.log.error('setActive', e)
-      callback(e)
-    }
-  }
-  
-  async getLockPhysicalControls(callback) {
-    try {
-      const [ child_lock ] = await this.device.call('get_prop', ['child_lock'])
-
-      callback(
-        null,
-        (child_lock === 'on')
-          ? Characteristic.LockPhysicalControls.CONTROL_LOCK_ENABLED
-          : Characteristic.LockPhysicalControls.CONTROL_LOCK_DISABLED
-      )
-    } catch (e) {
-      this.log.error('getLockPhysicalControls', e)
-      callback(e)
-    }
-  }
-
-  async setLockPhysicalControls(state, callback) {
-    try {
-      const child_lock = (state === Characteristic.LockPhysicalControls.CONTROL_LOCK_ENABLED)
-        ? 'on'
-        : 'off'
-
-      const [ result ] = await this.device.call('set_child_lock', [child_lock])
-
-      if (result !== 'ok')
-        throw new Error(result)
-
-      callback()
-    } catch (e) {
-      this.log.error('setLockPhysicalControls', e)
-      callback(e)
-    }
-  }
-  
-  async getCurrentTemperature(callback) {
-    try {
-      const [ temperature ] = await this.device.call('get_prop', ['temperature'])
-      callback(null, temperature)
-    } catch (e) {
-      this.log.error('getCurrentTemperature', e)
-      callback(e)
-    }
-  }
-  
-  async getCurrentHeaterCoolerState(callback) {
-    try {
-      const [ power ] = await this.device.call('get_prop', ['power'])
-      callback(
-        null,
-        (power === 'on')
-          ? Characteristic.CurrentHeaterCoolerState.HEATING
-          : Characteristic.CurrentHeaterCoolerState.INACTIVE
-      )
-    } catch (e) {
-      this.log.error('getCurrentHeaterCoolerState', e)
-      callback(e)
-    }
-  }
-  
-  async getCurrentRelativeHumidity(callback) {
-    try {
-      const [ humidity ] = await this.device.call('get_prop', ['relative_humidity'])
-      callback(null, humidity)
-    } catch (e) {
-      this.log.error('getCurrentRelativeHumidity', e)
-      callback(e)
-    }
-  }
-  
-  async getHeatingThresholdTemperature(callback) {
-    try {
-      const [ target_temperature ] = await this.device.call('get_prop', ['target_temperature'])
-      callback(null, target_temperature)
-    } catch (e) {
-      this.log.error('getHeatingThresholdTemperature', e)
-      callback(e)
-    }
-  }
-  async setHeatingThresholdTemperature(value, callback) {
-	  try {
-      const [ result ] = await this.device.call('set_target_temperature', [value])
-      if (result !== 'ok')
-        throw new Error(result)
-      callback()	
-    } catch (e) {
-      this.log.error('setHeatingThresholdTemperature', e)
-      callback(e)
-    }
-  }	  
+	} 	
 }
